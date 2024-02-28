38fd190d364e0b34ddf30b7f1a7f96fbb90451a5
Jared <jaredtponting@gmail.com>
2024-02-28 21:36:23 +0000

    initiates wfc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 import pygame
 import pygame.draw
@@ -27,6 +28,11 @@ def main():
     pygame.display.set_caption('wave-function-collapse')
     screen.fill(BLACK)
 
+    TILES_PER_AXIS = 2
+
+    def scale_image(img):
+        return pygame.transform.scale(img, (SCREEN_WIDTH / TILES_PER_AXIS, SCREEN_WIDTH / TILES_PER_AXIS))
+
     def draw_grid(n):
         """
         draws grid on screen
@@ -39,11 +45,26 @@ def main():
 
         for x in range(n):
             for y in range(n):
-                rect = pygame.Rect(x*block_width, y*block_width, block_width, block_width)
+                rect = pygame.Rect(x * block_width, y * block_width, block_width, block_width)
                 pygame.draw.rect(screen, WHITE, rect, 1)
 
+    def initiate_collapse():
+        """
+        Take random tile_plot and add a random tile
+        :return:
+        """
+        first_tile = TILES[random.randint(0, len(TILES) - 1)]
+        x = random.randint(0, TILES_PER_AXIS - 1)
+        y = random.randint(0, TILES_PER_AXIS - 1)
+
+        screen.blit(scale_image(first_tile.image), (SCREEN_WIDTH * x / TILES_PER_AXIS, SCREEN_WIDTH * y / TILES_PER_AXIS))
+
+    draw_grid(TILES_PER_AXIS)
+
+    initiate_collapse()
+
     while True:
-        draw_grid(30)
+        draw_grid(TILES_PER_AXIS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
