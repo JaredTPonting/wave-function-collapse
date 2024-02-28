56fa8327876b310d84c1f4aef6c99e862f4e7ff5
Jared <jaredtponting@gmail.com>
2024-02-28 21:39:49 +0000

    tile name

--- tile_class.py
+++ tile_class.py
@@ -2,6 +2,7 @@ import pygame
 
 
 class Tile:
-    def __init__(self, img_path, data):
+    def __init__(self, img_path, data, name):
         self.image = pygame.image.load(img_path)
         self.data = data
+        self.name = name
